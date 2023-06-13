15f9f95620c9774744ff9e7d774efdb69e088337
pavel shumikhin <shumaken141294@gmail.com>
2023-06-13 18:06:52 +0200

    удалены лишние комментарии

--- InputXmlFile/MainWindow.xaml.cs
+++ InputXmlFile/MainWindow.xaml.cs
@@ -19,9 +19,6 @@ using System.Windows.Shapes;
 
 namespace InputXmlFile
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         //Строка подключения к БД
